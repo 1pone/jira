741e8856d1e3843ac7f08c0a227bcef284878925
1pone <wellpeng97@qq.com>
2021-10-06 18:33:16 +0800

    chore: auth-provider

---
+++ src/auth-provider.ts
@@ -0,0 +1,40 @@
+import { User } from "screen/project-list/search-panel";
+const apiUrl = process.env.REACT_APP_API_URL;
+
+const localStorageKey = "__auth_provider_token__";
+
+export const getToken = () => window.localStorage.getItem(localStorageKey);
+
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+export const login = async (data: { username: string; password: string }) => {
+  const res = await fetch(`${apiUrl}/login`, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  if (res.ok) {
+    return handleUserResponse(await res.json());
+  }
+};
+
+export const register = async (data: {
+  username: string;
+  password: string;
+}) => {
+  const res = await fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  if (res.ok) {
+    return handleUserResponse(await res.json());
+  }
+};
+
+export const logout = () => {
+  window.localStorage.removeItem(localStorageKey);
+};
--- src/screen/login/index.tsx
+++ src/screen/login/index.tsx
@@ -1,17 +1,8 @@
 import * as React from "react";
 import { FormEvent } from "react";
-const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginScreen = () => {
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiUrl}/login`, {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify(param),
-    }).then(async (res) => {
-      console.log(res);
-    });
-  };
+  const login = (param: { username: string; password: string }) => {};
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const username = (event.currentTarget.elements[0] as HTMLInputElement)
